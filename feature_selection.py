4f74425d5e5a52fdc24f4a1968e17fc7662a294e
name <ch.anuraan@gmail.com>
2018-09-24 00:50:48 +0530

    Results weka

--- feature_selection.py
+++ feature_selection.py
@@ -5,20 +5,17 @@ import numpy as np
 from sklearn.feature_selection import mutual_info_classif
 from sklearn.feature_selection import SelectKBest
 
-def select_and_write(filename):
-	print(filename)
+def select_and_write(filename,num_feat):
+	print(filename+' '+str(num_feat))
 	# Read dataset
-	outfile='../featureSelected/'+filename
-	nummFeatures=10
-
-
+	outfile='../featureSelected/'+str(num_feat)+'/'+filename
 	df=pd.read_csv('../MicroarrayData/'+filename)
 	y=df['class']
 	df.drop('class',axis=1,inplace=True)
 	X=df
 
 	# perform feature selection
-	selector=SelectKBest(score_func=mutual_info_classif,k=10)
+	selector=SelectKBest(score_func=mutual_info_classif,k=num_feat)
 	X=selector.fit_transform(X,y)
 	X=pd.DataFrame(X)
 	X['class']=y
@@ -27,9 +24,12 @@ def select_and_write(filename):
 	# write to csv
 	X.to_csv(outfile,index=False)
 
+num_features=[10,50,100]
+
 # get list of files
 os.chdir('MicroarrayData/')
 filelist=glob.glob('*')
 
-for files in filelist:
-	select_and_write(files)
+for nf in num_features:
+	for files in filelist:
+		select_and_write(files,nf)
